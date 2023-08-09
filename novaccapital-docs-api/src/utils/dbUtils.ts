b668f6b490871dc65a3cdcecfbaf26ab28a56ebd
Mario Orellana <mario@journeyhero.net>
2023-08-09 11:30:57 -0600

    Implement update progress functions on doc creation

--- novaccapital-docs-api/src/utils/dbUtils.ts
+++ novaccapital-docs-api/src/utils/dbUtils.ts
@@ -13,11 +13,28 @@ const db = knex({
 
 const TableName = "document";
 
+async function getCount(): Promise<number> {
+    let requirements = await db("requirements").select().count("* as count").first();
+    return Number(requirements?.count);
+}
+
+async function getCountByApp(guid: string, applicationId: string): Promise<number> {
+    let path = `docs/${guid}/${applicationId}/%`;
+    let document = await db(TableName).select().where('path', 'LIKE', path).count("* as count").first();
+    return Number(document?.count);
+}
+
+async function updateProgress(id: string, progress: number) {
+    await db(TableName).where({ id: id }).update({ progress: progress });
+}
+
 export async function create(guid: string, applicationId: string, name: string) {
     const path = `docs/${guid}/${applicationId}/${name}`;
     const createdDocument = await db(TableName).insert({
         application_id: applicationId, name, path: path
     });
+    let progress = (await getCountByApp(guid, applicationId))/(await getCount());
+    await updateProgress(applicationId, progress);
     return createdDocument;
 }
 
