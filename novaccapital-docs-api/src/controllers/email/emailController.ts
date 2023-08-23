91b6c41d2e56d08ab08584815151f7c14a3338db
Mario Orellana <mario@journeyhero.net>
2023-08-23 11:41:30 -0600

    Add logs to email controller

--- novaccapital-docs-api/src/controllers/email/emailController.ts
+++ novaccapital-docs-api/src/controllers/email/emailController.ts
@@ -5,10 +5,10 @@ import approved from "./templates/approved";
 import denied from "./templates/denied";
 
 export const sendAppApprovedStatus = async (req: Request, res: Response) => {
+    console.log("Request to: /application/approved");
+    console.log("\t-Request body:", req.body);
     try {
         const { email, userName } = req.body;
-
-        console.log("Request to: /application/approved");
 
         if (!(email && userName)) {
             res.status(400).json({ message: "Expected attributes: email and userName" });
@@ -17,19 +17,20 @@ export const sendAppApprovedStatus = async (req: Request, res: Response) => {
 
         await sendHtmlEmail(email, "Aprobación de solicitud", approved(userName));
 
-        console.log("Email sent successfully");
+        console.log("\t-Email sent successfully");
         res.status(200).json({ message: "Email sent successfully" });
     } catch (error) {
         console.error("Error sending email:", error);
         res.status(500).json({ message: "Error sending email" });
     }
+    console.log("\t-Function end");
 };
 
 export const sendDocDeniedStatus = async (req: Request, res: Response) => {
+    console.log("Request to: /docs/denied");
+    console.log("\t-Request body:", req.body);
     try {
         const { email, userName, fileName, reason } = req.body;
-
-        console.log("Request to: /docs/denied");
 
         if (!(email && userName && fileName && reason)) {
             res.status(400).json({ message: "Expected attributes: email, userName, fileName and reason" });
@@ -38,12 +39,13 @@ export const sendDocDeniedStatus = async (req: Request, res: Response) => {
 
         await sendHtmlEmail(email, "Denegación del Documento en el Checklist", denied(userName, fileName, reason));
 
-        console.log("Email sent successfully");
+        console.log("\t-Email sent successfully");
         res.status(200).json({ message: "Email sent successfully" });
     } catch (error) {
         console.error("Error sending email:", error);
         res.status(500).json({ message: "Error sending email" });
     }
+    console.log("\t-Function end");
 };
 
 export default { sendDocDeniedStatus };
